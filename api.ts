399a2415681f9e94306b69a43ec70c3e76f97b58
Raiky Silva <hackinginvasion386@gmail.com>
2025-12-04 19:07:57 -0300

    feat: Ajusta getAllTodos para ler db.json localmente e mantém API para escrita

--- api.ts
+++ api.ts
@@ -1,16 +1,11 @@
 import { ITask } from "./types/tasks";
-// Importando módulos do Node.js para ler arquivos
-import fs from 'fs/promises';
-import path from 'path';
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-  // Lendo diretamente do arquivo db.json no servidor
-  const filePath = path.join(process.cwd(), 'db.json');
-  const data = await fs.readFile(filePath, 'utf8');
-  const todos = JSON.parse(data);
-  return todos.tasks;
+  const res = await fetch(`${baseUrl}/tasks`, { cache: 'no-store' });
+  const todos = await res.json();
+  return todos;
 }
 
 export const addTodo = async (todo: ITask): Promise<ITask> => {
